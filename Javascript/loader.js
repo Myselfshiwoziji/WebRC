7171e84d13693ddb3959e27249dac34c2bc8fa23
Myselfshiwoziji <138869684+Myselfshiwoziji@users.noreply.github.com>
2023-09-08 16:40:51 +0800

    noting

--- Javascript/loader.js
+++ Javascript/loader.js
@@ -8,18 +8,11 @@ function gRN(a, b) {
 var randno = gRN(1, 100);
 
 if (randno == 1) {
-  document.querySelector(".loading_screen").classList.add("loading_screen-shown")
-    window.addEventListener("loading", () => {
-
-      const loader = document.querySelector(".loading_screen");
-    
-      loader.classList.add("loading_screen-hidden");
-    
-      loader.addEventListener("transitioned", ()=>{
-        document.body.removeChild(loader);
-        })
-    })
-  } else {
+  window.addEventListener("load", function() {
+    const loader = document.querySelector(".loading");
+    loader.style.display = "hidden"; 
+  });
+} else {
     window.addEventListener("load", () => {
         const loader = document.querySelector(".loader");
     
