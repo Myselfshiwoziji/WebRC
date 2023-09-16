7be1ca0241bb7061157186bfc3a9b979d0ea5c3e
ColKol <coliemail.07@gmail.com>
2023-09-16 12:26:55 +0800

    experimenting with global menu

--- Javascript/sidemenu.js
+++ Javascript/sidemenu.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the header HTML file
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "menu.html", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Insert the header HTML into the page
+        const header = xhr.responseText;
+        const headerElement = document.getElementById("main_side")
+        headerElement.innerHTML = header;
+      }
+    };
+    xhr.send();
+    dropping_time();
+});
+
 function openNav() {
     document.getElementById("main_side").style.width = "300px";
 }
