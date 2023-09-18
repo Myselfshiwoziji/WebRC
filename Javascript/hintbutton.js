bad7d3f5eea2c04782780f9cd851a425f8a5a3bf
Myselfshiwoziji <138869684+Myselfshiwoziji@users.noreply.github.com>
2023-09-18 11:28:18 +0800

    lesson 5

--- Javascript/hintbutton.js
+++ Javascript/hintbutton.js
@@ -1,9 +1,1 @@
-function buttonclick() {
-  if (paragraph.style.display === 'none') {
-    paragraph.style.display = 'block';
-    paragraph.style.visibility = "visible";
-  } else {
-    paragraph.style.display = 'none';
-    paragraph.style.visibility = "hidden";
-  }
-}  
+
